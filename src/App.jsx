8064e41756c64a111d0228935aac10a386bf70ff
Tejaswi <tejaswiponduri77@gmail.com>
2026-09-20 11:26:57 +0530

    Remove unwanted folder

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import './App.css'
+ 
+import Demo1 from "./pages/Demo1"
+import Demo2 from "./pages/Demo2"
+import Demo3 from "./pages/Demo3"
+
+function App() 
+{
+ 
+  return (
+    <div>
+      <h2 style={{textAlign:"center",color:"red"}}>
+            <u>Sample React App</u>
+      </h2>
+      <Demo1 />
+      <Demo2 />
+      <Demo3 />
+    </div>
+  )
+}
+ 
+export default App
---
+++ src/pages/Demo3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Demo3() {
+  return (
+    <div>
+        <h3>Class Component Demo</h3>
+        <p>FSAD</p>
+    </div>
+  )
+}
