8a295063b92d4149763e15b8f8d894869ac8e1e1
Tejaswi <tejaswiponduri77@gmail.com>
2026-09-20 12:06:14 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,20 @@ import Demo4 from "./pages/Demo4"
 
 function App() 
 {
- 
+  let a = 10         
+  let txt = "jfsd"
+  let emp = {"id":1, "name":"Ram", "salary":80000, "gender":"male"}
+
   return (
     <div>
       <h2 style={{textAlign:"center",color:"red"}}>
-            <u>Sample React App</u>
+        <u>Sample React App</u>
       </h2>
+
       <Demo1 />
       <Demo2 />
       <Demo3 />
-      <Demo4 a={a} b={100} text={txt} emp={emp}/>
+      <Demo4 a = {a} b = {100} text = {txt} emp = {emp}/>
     </div>
   )
 }
