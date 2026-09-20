f9ca3752c8907b5f4a54ed6bda93f4d66b69bb6c
Tejaswi <tejaswiponduri77@gmail.com>
2026-09-20 11:37:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@
 import Demo1 from "./pages/Demo1"
 import Demo2 from "./pages/Demo2"
 import Demo3 from "./pages/Demo3"
+import Demo4 from "./pages/Demo4"
 
 function App() 
 {
@@ -19,6 +20,7 @@ function App()
       <Demo1 />
       <Demo2 />
       <Demo3 />
+      <Demo4 a={a} b={100} text={txt} emp={emp}/>
     </div>
   )
 }
